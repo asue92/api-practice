a3e3c358b899b5fc0470f8cb519956dd241efc38
Adam Sue <adam.p.sue@gmail.com>
2022-03-02 21:22:48 -0500

    added express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 4041;
+
+app.get("/", (_, res) => {
+  res.send("Your Express App");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
